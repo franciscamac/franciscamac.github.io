9499f4ddd2eea136281bcb7877e76570f84e39f5
francisca.macario <francisca.macario@defensoria.pi.def.br>
2024-10-31 11:37:15 -0300

    Deploy da aplicação web no GitHub Pages

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var video = document.getElementById("background-video");
+    video.playbackRate = 0.50; // Define o vídeo para reproduzir a 50% da velocidade
+});
